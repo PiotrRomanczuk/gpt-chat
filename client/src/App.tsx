732f0f89d386a698c741b059e603e78379c6a31d
PiotrRomanczuk <p.romanczuk@gmail.com>
2023-06-16 16:57:08 +0200

    .

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,11 @@
 import './App.css'
-import { useState } from 'react';
 
-import Input from './components/UI/InputComponent';
-import Button from './components/UI/ButtonComponent';
 import ChatComponent from './components/Logic/ChatComponent';
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
 
-  const [response, setResponse] = useState('');
-
-  const handleButtonClick = () => {  };
-  
-  
   return (
     <div>
-      {/* <Input value={inputValue} onChange={setInputValue} /> */}
-      {/* <Button onClick={handleButtonClick} title={ "Fetch"} /> */}
       <ChatComponent />
     </div>
   );
