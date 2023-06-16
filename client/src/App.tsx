eaaafba10a73f26b051a4a73f8406b9b701a4b3e
PiotrRomanczuk <p.romanczuk@gmail.com>
2023-06-16 16:56:08 +0200

    Added readMe && splitted client components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,50 +1,23 @@
 import './App.css'
 import { useState } from 'react';
 
-import Input from './components/Input';
-import Button from './components/Button';
+import Input from './components/UI/InputComponent';
+import Button from './components/UI/ButtonComponent';
+import ChatComponent from './components/Logic/ChatComponent';
 
 function App() {
   const [inputValue, setInputValue] = useState('');
 
   const [response, setResponse] = useState('');
 
-  const handleButtonClick = () => {
-
-    // get the input value
-    
-    // make an async function call to the API
-
-    // update the state with the response from the API
-
-
-  };
+  const handleButtonClick = () => {  };
   
-
-
-
-      // fetch('/api/endpoint', {
-      //   method: 'POST',
-      //   body: JSON.stringify({ data: inputValue }),
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //   },
-      // })
-      //   .then((response) => response.json())
-      //   .then((data) => {
-  
-      //     console.log(data);
-      //   })
-      //   .catch((error) => {
-  
-      //     console.error(error);
-      //   });
-
   
   return (
     <div>
-      <Input value={inputValue} onChange={setInputValue} />
-      <Button onClick={handleButtonClick} />
+      {/* <Input value={inputValue} onChange={setInputValue} /> */}
+      {/* <Button onClick={handleButtonClick} title={ "Fetch"} /> */}
+      <ChatComponent />
     </div>
   );
 }
