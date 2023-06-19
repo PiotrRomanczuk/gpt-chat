e99ccdb337be603e330413cded2db29dba306ab0
PiotrRomanczuk <p.romanczuk@gmail.com>
2023-06-19 21:41:43 +0200

    Added errors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Packages
-const express = require('express');
 require('dotenv').config();
+require('express-async-errors');
+const express = require('express');
 const cors = require('cors');
 const auth = require("./middleware/auth");
 const bcrypt = require("bcryptjs");
