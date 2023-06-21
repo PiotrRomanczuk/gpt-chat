712c681fa7686bbf0321c4d94201d514b2229fb2
PiotrRomanczuk <p.romanczuk@gmail.com>
2023-06-21 14:20:49 +0200

    Added deletingUser function && writeToFile (not working yet) function

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,41 +1,45 @@
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const Login = async (req, res) => {
 
-  // Our login logic starts here
   try {
-    // Get user input
-    const { email, password} = req.body;
 
+    const { email, password } = req.body;
 
-    // Validate user input
     if (!(email && password)) {
       res.status(400).send("All input is required");
     }
 
-
-    // Validate if user exist in our database
     const user = await User.findOne({ email });
 
     if (user && (await bcrypt.compare(password, user.password))) {
+
       // Create token
-      const token = jwt.sign(
+      const token = jwt.verify(
         { user_id: user._id, email },
         process.env.TOKEN_KEY,
         {
-          expiresIn: "2h",
+          expiresIn: "1m",
         }
       );
-
+      
+      
       // save user token
       user.token = token;
 
-      // user
-      res.status(200).json(user);
-    }
-    res.status(400).send("Invalid Credentials");
-  } catch (err) {
-    console.log(err);
-  }
-  // Our register logic ends here
+      // Send user object as response
+      console.log("User logged in");
+      return res.status(200).json(user);
+}
+
+      // Invalid credentials response
+    return res.status(400).send("Invalid Credentials");
+    
+        } catch (err) {
+          console.log(err);
+        }
 }
 
 module.exports = Login;
